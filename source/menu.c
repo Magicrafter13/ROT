1a4415f8bf93ffd75176426e23c3922ff4aadb49
Magicrafter13 <scubaventure101@hotmail.com>
2017-06-12 13:52:41 -0700

    User Data reading changed.
    Instead of a LONG fscanf line I converted it to an Array that goes through the file and waits for matching strings.

--- source/menu.c
+++ source/menu.c
@@ -31,8 +31,84 @@ int games(char userDir[30])
 	settingsFile = fopen(sf, "r");
 	char dummy[30], userName[30];
 	int back, text, XP, XPEarned, lvl, Battleship, Blackjack, Chess, ConnectFour, DOND, FakeVirus, GTA, Sudoku, DodgeFall, Stress, Minesweeper, Mastermind, War, Nanogram, TTT, RC, RPS, Timer, WorkLock, Mancala, Monopoly, NGG, Journal, Pawn, SlotMach, Snake, Trivia, Alarm, msg;
-	fscanf(userFile, "%s %s %s %s %s %s %s %s %d %d %s %s %d %s %s %d %s %s %d", dummy, dummy, userName, dummy, dummy, dummy, dummy, dummy, &back, &text, dummy, dummy, &XP, dummy, dummy, &XPEarned, dummy, dummy, &lvl);
-	fscanf(userFile, "%s %s %d %s %s %d %s %s %d %s %s %d %s %s %d %s %s %d %s %s %d %s %s %d %s %s %d %s %s %d %s %s %d %s %s %d %s %s %d %s %s %d %s %s %d %s %s %d %s %s %d %s %s %d %s %s %d %s %s %d %s %s %d %s %s %d %s %s %d %s %s %d %s %s %d %s %s %d %s %s %d %s %s %d %s %s %d", dummy, dummy, &Battleship, dummy, dummy, &Blackjack, dummy, dummy, &Chess, dummy, dummy, &ConnectFour, dummy, dummy, &DOND, dummy, dummy, &FakeVirus, dummy, dummy, &GTA, dummy, dummy, &Sudoku, dummy, dummy, &DodgeFall, dummy, dummy, &Stress, dummy, dummy, &Minesweeper, dummy, dummy, &Mastermind, dummy, dummy, &War, dummy, dummy, &Nanogram, dummy, dummy, &TTT, dummy, dummy, &RC, dummy, dummy, &RPS, dummy, dummy, &Timer, dummy, dummy, &WorkLock, dummy, dummy, &Mancala, dummy, dummy, &Monopoly, dummy, dummy, &NGG, dummy, dummy, &Journal, dummy, dummy, &Pawn, dummy, dummy, &SlotMach, dummy, dummy, &Snake, dummy, dummy, &Trivia, dummy, dummy, &Alarm, dummy, dummy, &msg);
+	char otherArray[6][30];
+	strcpy(otherArray[0], "name");
+	strcpy(otherArray[1], "pass");
+	strcpy(otherArray[2], "color");
+	strcpy(otherArray[3], "XP");
+	strcpy(otherArray[4], "XPEarned");
+	strcpy(otherArray[5], "lvl");
+	for (int i = 0; i < 6; i++)
+	{
+		while(true)
+		{
+			fscanf(userFile, "%s", dummy);
+			if (strcmp(dummy, otherArray[i]) == 0)
+			{
+				if (i == 0)
+					fscanf(userFile, "%s %s", dummy, userName);
+				if (i == 2)
+					fscanf(userFile, "%s %d %d", dummy, &back, &text);
+				if (i == 3)
+					fscanf(userFile, "%s %d", dummy, &XP);
+				if (i == 4)
+					fscanf(userFile, "%s %d", dummy, &XPEarned);
+				if (i == 5)
+					fscanf(userFile, "%s %d", dummy, &lvl);
+				break;
+			}
+		}
+	}
+	//fscanf(userFile, "%s %s %s %s %s %s %s %s %d %d %s %s %d %s %s %d %s %s %d", dummy, dummy, userName, dummy, dummy, dummy, dummy, dummy, &back, &text, dummy, dummy, &XP, dummy, dummy, &XPEarned, dummy, dummy, &lvl);
+	char gameArray[14][30];
+	strcpy(gameArray[0], "Battleship");
+	strcpy(gameArray[1], "Blackjack");
+	strcpy(gameArray[2], "Chess");
+	strcpy(gameArray[3], "ConnectFour");
+	strcpy(gameArray[4], "Sudoku");
+	strcpy(gameArray[5], "DodgeFall");
+	strcpy(gameArray[6], "Minesweeper");
+	strcpy(gameArray[7], "Mastermind");
+	strcpy(gameArray[8], "War");
+	strcpy(gameArray[9], "Nanogram");
+	strcpy(gameArray[10], "Mancala");
+	strcpy(gameArray[11], "Monopoly");
+	strcpy(gameArray[12], "SlotMach");
+	strcpy(gameArray[13], "Snake");
+	for (int i = 0; i < 11; i++)
+	{
+		while(true)
+		{
+			fscanf(userFile, "%s", dummy);
+			int part = i + 1;
+			if (strcmp(dummy, gameArray[part]) == 0)
+			{
+				if (i == 0)
+					fscanf(userFile, "%s %d", dummy, &Battleship);
+				if (i == 1)
+					fscanf(userFile, "%s %d", dummy, &Blackjack);
+				if (i == 2)
+					fscanf(userFile, "%s %d", dummy, &Chess);
+				if (i == 3)
+					fscanf(userFile, "%s %d", dummy, &ConnectFour);
+				if (i == 4)
+					fscanf(userFile, "%s %d", dummy, &Sudoku);
+				if (i == 5)
+					fscanf(userFile, "%s %d", dummy, &DodgeFall);
+				if (i == 6)
+					fscanf(userFile, "%s %d", dummy, &Minesweeper);
+				if (i == 7)
+					fscanf(userFile, "%s %d", dummy, &Mastermind);
+				if (i == 8)
+					fscanf(userFile, "%s %d", dummy, &War);
+				if (i == 9)
+					fscanf(userFile, "%s %d", dummy, &Nanogram);
+				if (i == 10)
+					fscanf(userFile, "%s %d", dummy, &Mancala);
+				break;
+			}
+		}
+	}
 	fclose(userFile);
 	int gameMax = 10;
 	int gameMin = 0;
@@ -56,7 +132,7 @@ int games(char userDir[30])
 	printf(RESET " %sBlackjack    = %s " RESET "| %sMastermind  = %s\n", isSelected(2), isLocked(Blackjack), isSelected(3), isLocked(Mastermind));
 	printf(RESET " %sChess        = %s " RESET "| %sWar         = %s\n", isSelected(4), isLocked(Chess), isSelected(5), isLocked(War));
 	printf(RESET " %sConnect Four = %s " RESET "| %sNanogram    = %s\n", isSelected(6), isLocked(ConnectFour), isSelected(7), isLocked(Nanogram));
-	printf(RESET " %sSudoku       = %s " RESET "| %sTic Tac Toe = %s\n", isSelected(8), isLocked(Sudoku), isSelected(9), isLocked(TTT));
+	printf(RESET " %sSudoku       = %s " RESET "| %sMancala     = %s\n", isSelected(8), isLocked(Sudoku), isSelected(9), isLocked(Mancala));
 	printf(RESET " %sDodge Fall   = %s", isSelected(10), isLocked(DodgeFall));
 	while(true)
 	{
