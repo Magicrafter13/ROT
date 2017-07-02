c591cda389283703b496d013d179c3777cd32a6d
Magicrafter13 <scubaventure101@hotmail.com>
2017-07-02 09:22:26 -0700

    Added ZL ZR page moving to timeline
    Read the title.
    
    Oh, and next commit, I'm planning to add a scroll bar on screen (purely visual of course).

--- source/about.cpp
+++ source/about.cpp
@@ -98,18 +98,18 @@ char dispArray[10][51] =
 	"                                                  "
 };
 
-char timelineArray[10][769] =
+char timelineArray[10][792] =
 {
-	"[Jun 2017]--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------[Jul 2017]                                                                                                  ",
-	"     |                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   |                                                                                                      ",
-	"     +-----[ 6/06/17]---------------------------------------------[ 6/07/17]-----------------------------------------------------------------------------------------------------------------------------------------------------------[ 6/08/17]------------------------------------------------------------------------------[ 6/09/17]-----------------------[ 6/12/17]---------------------------------------------[ 6/13/17]-----------------------[ 6/14/17]-[ 6/15/17]-[ 6/16/17]-[ 6/19/17]-[ 6/22/17]-[ 6/24/17]-------------------------------------------------------------------[ 6/26/17]-[ 6/29/17]----------------------------------[ 6/30/17]                       [ 7/01/17]                                                                                                  ",
-	"                |                                                      |                                                                                                                                                                    |                                                                                       |                                |                                                      |                                |          |          |          |          |          |                                                                            |          |                                           |                                |                                                                                                      ",
-	"                +------ d42d9c0 -- 09b8859 -- 0c1cf90 -- 21dc50c       +------ d5c4cc8 -- 481f2ad -- 87bfbf0 -- d5674c6 -- 41985fe -- 3e50737 -- ee3dea8 -- a34a237 -- 820fe6d -- 370dd4e -- 80b7c51 -- 9399406 -- 50696a0 -- e1bcba5       +------ cc66dbb -- 1291e05 -- 33e0c28 -- 73bb65d -- e6df68b -- c6c1fe0 -- 32adbd8       +------ 15fb9b2 -- 3f97168       +------ ce5feaf -- 9addb0a -- 90e65e8 -- 1a4415f       +------ 2779ea3               b947377    0480203    ae26aae    7619981    6734a2d       +------ ab0e8c6 -- f66a570 -- 0ac5225 -- 9650fc6 -- 2a77db0 -- 724d973    b41a2d4       +------ 54f53f2 -- 888ebdb -- 71f038d       +------ f5377c7 -- 85b900c       +------ 37f62e8 -- fa916b5 -- 0cb5c5b -- a00cef1 -- 19c503b -- 34fe371 -- a2bcce4 -- fad3e1a -- current",
-	"                           |                     |                                                                            |                                           |                                                                            |          |                     |                     |                                           |                     |          |          |          |                     |                     |                                |          |                                |          |                     |                     |                     |          |          |                     |          |                     |          |          |          |          |          |                     |          |   ",
-	"                         A 1.3                 A 1.4                                                                          |                                         A 1.5                                                                        A 1.6        |                   A 1.7                   |                                           |                     ]----------+----[]----+----------[                   A 1.8                   |                                |        A 1.9                            A 1.10   A 1.11.2                  |                 A 1.11.3              A 1.16.6   A 1.16.7    B 1.0.1                  |       B 1.0.1                  |       B 1.0.2    B 1.0.3       ]----------+----------[                     |          |   ",
-	"                                                                                                                              |                                                                                                                                   |                                           |                                           |                                     []                                     |                     |                                |                                           |                                |                                           |                                           |                                |                                           |                                |          |   ",
-	"                       [Alpha 1.3 was the first time I had used GitHub.]                                              Settings Menu POC                                                    Between A 1.4 and A 1.5...                                 First GitHub related file.              Felt so stupid here. This commit was               First Issue I fixed     Although these commit's by themselves are boring     This is in my opinion   One of the first real   As you can see, commits became                   Is also A 1.10.1   This commit, the one before, and the one after   Second time having releases right after             FINALLY! Commits inBETWEEN             First dabble in          Haven't done commits this close together   Began work on timeline   Added",
-	"                 I do have cia files for Alpha 1.0 - 1.2, and may release them someday.            I remember thinking this was awesome, now we have XP Store               I wish I still made that many commit's between versions                          [Readme.md]             literally just the 3dsx files from the previous commit             [#1]                      They lead up to....: [Right -->]        The first major/cool release.   features (change pass)   infrequent. Day by day if that.           But got mixed up somehow   Just contained some of GitHub's 'MD' files.   each other. First was 1.10.1 to 1.11.2   Releases (and breathing room for the timeline)   Random numbers.    In a while. (Less than an hour apart I believe.)   ;p        timeline feature"
+	"[Jun 2017]--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------[Jul 2017]                                                                                                                         ",
+	"     |                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   |                                                                                                                             ",
+	"     +-----[ 6/06/17]---------------------------------------------[ 6/07/17]-----------------------------------------------------------------------------------------------------------------------------------------------------------[ 6/08/17]------------------------------------------------------------------------------[ 6/09/17]-----------------------[ 6/12/17]---------------------------------------------[ 6/13/17]-----------------------[ 6/14/17]-[ 6/15/17]-[ 6/16/17]-[ 6/19/17]-[ 6/22/17]-[ 6/24/17]-------------------------------------------------------------------[ 6/26/17]-[ 6/29/17]----------------------------------[ 6/30/17]                       [ 7/01/17]----------------------------------------------------------------------------------------------------[ 7/02/17]           ",
+	"                |                                                      |                                                                                                                                                                    |                                                                                       |                                |                                                      |                                |          |          |          |          |          |                                                                            |          |                                           |                                |                                                                                                             |               ",
+	"                +------ d42d9c0 -- 09b8859 -- 0c1cf90 -- 21dc50c       +------ d5c4cc8 -- 481f2ad -- 87bfbf0 -- d5674c6 -- 41985fe -- 3e50737 -- ee3dea8 -- a34a237 -- 820fe6d -- 370dd4e -- 80b7c51 -- 9399406 -- 50696a0 -- e1bcba5       +------ cc66dbb -- 1291e05 -- 33e0c28 -- 73bb65d -- e6df68b -- c6c1fe0 -- 32adbd8       +------ 15fb9b2 -- 3f97168       +------ ce5feaf -- 9addb0a -- 90e65e8 -- 1a4415f       +------ 2779ea3               b947377    0480203    ae26aae    7619981    6734a2d       +------ ab0e8c6 -- f66a570 -- 0ac5225 -- 9650fc6 -- 2a77db0 -- 724d973    b41a2d4       +------ 54f53f2 -- 888ebdb -- 71f038d       +------ f5377c7 -- 85b900c       +------ 37f62e8 -- fa916b5 -- 0cb5c5b -- a00cef1 -- 19c503b -- 34fe371 -- a2bcce4 -- fad3e1a -- 11b0bb6       +------ current ",
+	"                           |                     |                                                                            |                                           |                                                                            |          |                     |                     |                                           |                     |          |          |          |                     |                     |                                |          |                                |          |                     |                     |                     |          |          |                     |          |                     |          |          |          |          |          |                     |          |                          ",
+	"                         A 1.3                 A 1.4                                                                          |                                         A 1.5                                                                        A 1.6        |                   A 1.7                   |                                           |                     ]----------+----[]----+----------[                   A 1.8                   |                                |        A 1.9                            A 1.10   A 1.11.2                  |                 A 1.11.3              A 1.16.6   A 1.16.7    B 1.0.1                  |       B 1.0.1                  |       B 1.0.2    B 1.0.3       ]----------+----------[                     |          |                          ",
+	"                                                                                                                              |                                                                                                                                   |                                           |                                           |                                     []                                     |                     |                                |                                           |                                |                                           |                                           |                                |                                           |                                |          |                          ",
+	"                       [Alpha 1.3 was the first time I had used GitHub.]                                              Settings Menu POC                                                    Between A 1.4 and A 1.5...                                 First GitHub related file.              Felt so stupid here. This commit was               First Issue I fixed     Although these commit's by themselves are boring     This is in my opinion   One of the first real   As you can see, commits became                   Is also A 1.10.1   This commit, the one before, and the one after   Second time having releases right after             FINALLY! Commits inBETWEEN             First dabble in          Haven't done commits this close together   Began work on timeline   Added                       ",
+	"                 I do have cia files for Alpha 1.0 - 1.2, and may release them someday.            I remember thinking this was awesome, now we have XP Store               I wish I still made that many commit's between versions                          [Readme.md]             literally just the 3dsx files from the previous commit             [#1]                      They lead up to....: [Right -->]        The first major/cool release.   features (change pass)   infrequent. Day by day if that.           But got mixed up somehow   Just contained some of GitHub's 'MD' files.   each other. First was 1.10.1 to 1.11.2   Releases (and breathing room for the timeline)   Random numbers.    In a while. (Less than an hour apart I believe.)          ;p       timeline feature                 "
 };
 
 int timelineDisplay()
@@ -143,7 +143,7 @@ int timelineDisplay()
 			printf(COLOR YELLOW CEND);
 		printf("%s", dispArray[i]);
 	}
-	printf("\n" COLOR CYAN CEND "           Use Left and Right to move.\n           Or use L and R to fast move.\n");
+	printf("\n" COLOR CYAN CEND "           Use Left and Right to move.\n           Or use L and R to fast move.\n        Or use ZL and ZR to move one page.");
 	return 0;
 }
 
@@ -153,7 +153,7 @@ int timeline(int upperrv)
 	char returnvalue[30];
 	strcpy(returnvalue, "ERROR");
 	int tlMin = 0;
-	int tlMax = 718;
+	int tlMax = 741;
 	consoleSelect(&bottomScreen);
 	if (upperrv != 1)
 		if (debugTF)
@@ -171,31 +171,35 @@ int timeline(int upperrv)
 		u32 kHeld = hidKeysHeld();
 		if (selTimeline > tlMin)
 		{
-			if (kDown & KEY_LEFT)
+			if (kDown & KEY_LEFT || kHeld & KEY_L)
 			{
 				sprintf(returnvalue, "LEFT");
 				selTimeline--;
 				break;
 			}
-			if (kHeld & KEY_L)
+			if (kDown & KEY_ZL)
 			{
-				sprintf(returnvalue, "L");
-				selTimeline--;
+				sprintf(returnvalue, "LEFT");
+				selTimeline -= 50;
+				if (selTimeline < tlMin)
+					selTimeline = tlMin;
 				break;
 			}
 		}
 		if (selTimeline < tlMax)
 		{
-			if (kDown & KEY_RIGHT)
+			if (kDown & KEY_RIGHT || kHeld & KEY_R)
 			{
 				sprintf(returnvalue, "RIGHT");
 				selTimeline++;
 				break;
 			}
-			if (kHeld & KEY_R)
+			if (kDown & KEY_ZR)
 			{
-				sprintf(returnvalue, "R");
-				selTimeline++;
+				sprintf(returnvalue, "RIGHT");
+				selTimeline += 50;
+				if (selTimeline > tlMax)
+					selTimeline = tlMax;
 				break;
 			}
 		}
@@ -230,7 +234,7 @@ int timeline(int upperrv)
 	}
 	if (strcmp(returnvalue, "START") == 0)
 		ireturnvalue = 2;
-	if ((strcmp(returnvalue, "LEFT") == 0) || (strcmp(returnvalue, "RIGHT") == 0) || (strcmp(returnvalue, "L") == 0) || (strcmp(returnvalue, "R") == 0))
+	if ((strcmp(returnvalue, "LEFT") == 0) || (strcmp(returnvalue, "RIGHT") == 0))
 		ireturnvalue = 1;
 	if (strcmp(returnvalue, "B") == 0)
 		ireturnvalue = 0;
